aa27242d2c44ed11e96611c9cdd1447d037d7e44
ninaw04 <ninawangcn@gmail.com>
2024-04-14 15:48:05 -0700

    :truck: refactored into routes

--- server/index.js
+++ server/index.js
@@ -1,86 +1,15 @@
-// imports aren't allowed since this isn't a module
-const config = require('../.userConfig')
 const express = require('express')
 const app = express()
-const mysql = require('mysql')
 const PORT = 8080;
 const cors = require('cors')
 
-const db = mysql.createConnection( config.connect )
+var booksRouter = require("./routes/books");
+var readingRouter = require("./routes/readingList");
 
 app.use(express.json())
+app.use("/", booksRouter);
+app.use("/reading", readingRouter);
 app.use(cors())
-
-app.get('/', (req, res) => {
-    res.json('Backend for home library catalogue')
-})
-
-// Create
-app.post('/books', (req, res) => {
-    const q = 'INSERT INTO Books (`title`, `author`, `genre`, `lang`, `location`, `cover`) VALUES (?)'
-    const values = [
-        req.body.title,
-        req.body.author,
-        req.body.genre,
-        req.body.lang,
-        req.body.location,
-        req.body.cover
-    ]
-
-    db.query(q, [values], (err, data) => {
-        if(err) return res.json(err)
-        return res.json("Book has been created")
-    })
-})
-
-// Read 
-app.get('/books', (req, res) => {
-    const q = 'SELECT * FROM Books'
-
-    db.query(q, (err, data) => {
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
-
-// Get specific book
-app.get('/books/:id', (req, res) => {
-    const {id} = req.params
-    const q = `SELECT * FROM Books where BookID = ${id}`
-
-    db.query(q, (err, data) => {
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
-
-// Specific Title for search purposes?
-app.get('/books/:id/title', (req, res) => {
-    const {id} = req.params
-    const q = `SELECT Title FROM Books where BookID = ${id}`
-
-    db.query(q, (err, data) => {
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
-
-// Update
-app.patch('/books/:id', (req, res) => {
-    const {id} = req.params
-})
-
-
-// Delete
-app.delete('/books/:id', (req, res) => {
-    const bookID = req.params.id
-    const q = 'DELETE FROM books WHERE BookID = ?'
-
-    db.query(q, [bookID], (err, data) => {
-        if(err) return res.json(err)
-        return res.json("Book has been deleted")
-    })
-})
 
 app.listen(PORT, () => {
     console.log(`connected on http://localhost:${PORT}`)
---
+++ server/routes/books.js
@@ -0,0 +1,94 @@
+var express = require("express");
+var router = express.Router();
+var cors = require('cors');
+const config = require('../../.userConfig')
+
+router.use(cors());
+
+let db = require('../database').databaseConnection
+
+router.get('/', (req, res) => {
+  res.json('Backend for home library catalogue')
+})
+
+// Create book
+router.post('/books', (req, res) => {
+  const q = 'INSERT INTO Books (`title`, `author`, `genre`, `lang`, `location`, `cover`) VALUES (?)'
+  const values = [
+      req.body.title,
+      req.body.author,
+      req.body.genre,
+      req.body.lang,
+      req.body.location,
+      req.body.cover
+  ]
+
+  db.query(q, [values], (err, data) => {
+      if(err) return res.json(err)
+      return res.json("Book has been created")
+  })
+})
+
+// Read (get) book
+router.get('/books', (req, res) => {
+  const q = 'SELECT * FROM Books'
+
+  db.query(q, (err, data) => {
+      if(err) return res.json(err)
+      return res.json(data)
+  })
+})
+
+// Get specific book
+router.get('/books/:id', (req, res) => {
+  const {id} = req.params
+  const q = `SELECT * FROM Books where BookID = ${id}`
+
+  db.query(q, (err, data) => {
+      if(err) return res.json(err)
+      return res.json(data)
+  })
+})
+
+// Specific Title for search purposes?
+router.get('/books/:id/title', (req, res) => {
+  const {id} = req.params
+  const q = `SELECT Title FROM Books where BookID = ${id}`
+
+  db.query(q, (err, data) => {
+      if(err) return res.json(err)
+      return res.json(data)
+  })
+})
+
+// Update Book
+router.put('/books/:id', (req, res) => {
+  const bookID = req.params.id
+  const q = "UPDATE books SET `Title` = ?, `Author` = ?, `Genre` = ?, `Lang` = ?, `Location` = ?, `Cover` = ? WHERE BookID = ? "
+  const values = [
+      req.body.title,
+      req.body.author,
+      req.body.genre,
+      req.body.lang,
+      req.body.location,
+      req.body.cover
+  ]
+
+  db.query(q, [...values, bookID], (err, data) => { 
+      if(err) return res.json(err)
+      return res.json("Book has been updated")
+  })
+})
+
+// Delete Book 
+router.delete('/books/:id', (req, res) => {
+  const bookID = req.params.id
+  const q = 'DELETE FROM books WHERE BookID = ?'
+
+  db.query(q, [bookID], (err, data) => {
+      if(err) return res.json(err)
+      return res.json("Book has been deleted")
+  })
+})
+
+module.exports = router;
